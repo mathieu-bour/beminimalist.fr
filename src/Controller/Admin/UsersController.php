0a1c5e8c075f151de7823b51a2879b01e5fa769e
Mathieu Bour <mathieu.tin.bour@gmail.com>
2016-12-11 18:00:53 +0100

    REST API: Users login

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -12,11 +12,18 @@ class UsersController extends AppController
      * =========================================================== */
     public function login()
     {
+        if($this->request->is('json')) {
+            return $this->_login();
+        }
+
         $this->viewBuilder()->layout('default_centred');
 
         if ($this->Auth->user()) {
             $this->Flash->error('Vous êtes déjà connecté');
-            $this->redirect(['controller' => 'pages', 'action' => 'dashboard']);
+            $this->redirect([
+                'controller' => 'pages',
+                'action'     => 'dashboard'
+            ]);
         }
 
         if ($this->request->is('post')) {
@@ -25,7 +32,10 @@ class UsersController extends AppController
             if ($user) {
                 $this->Auth->setUser($user);
 
-                $this->redirect(['controller' => 'pages', 'action' => 'dashboard']);
+                $this->redirect([
+                    'controller' => 'pages',
+                    'action'     => 'dashboard'
+                ]);
             } else {
                 $this->Flash->error('Identifiants incorrects');
             }
@@ -36,7 +46,11 @@ class UsersController extends AppController
     {
         $this->Flash->success('Vous êtes maintenant déconnecté.');
         $this->get('session')->clear();
-        return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+
+        return $this->redirect([
+            'controller' => 'Users',
+            'action'     => 'login'
+        ]);
     }
 
 
@@ -60,13 +74,19 @@ class UsersController extends AppController
                 $this->Flash->error('Erreur lors de l\'enregistrement de l\'utilistaeur');
             }
 
-            $this->redirect(['controller' => 'Users', 'action' => 'index']);
+            $this->redirect([
+                'controller' => 'Users',
+                'action'     => 'index'
+            ]);
         }
     }
 
     public function delete($id)
     {
-        $this->request->allowMethod(['post', 'delete']);
+        $this->request->allowMethod([
+            'post',
+            'delete'
+        ]);
         $earlyCode = $this->Users->get($id);
         if ($this->Users->delete($earlyCode)) {
             $this->Flash->success('L\'utilisateur a bien été supprimé');
@@ -74,6 +94,30 @@ class UsersController extends AppController
             $this->Flash->error('Il y a eu un problème lors de la suppression de l\'utilisateur');
         }
 
-        $this->redirect(['controller' => 'Users', 'action' => 'index']);
+        $this->redirect([
+            'controller' => 'Users',
+            'action'     => 'index'
+        ]);
+    }
+
+    /* = REST API
+     * =========================================================== */
+    private function _login()
+    {
+        $this->request->allowMethod('post');
+
+        $user = $this->Auth->user() ? $this->Auth->user() : $this->Auth->identify();
+
+        if(!$user) {
+            return $this->json([
+               'message' => 'Identifiants incorrects',
+               'code' => 403
+            ]);
+        } else {
+            $this->Auth->setUser($user);
+            return $this->json([
+                'message' => 'Connexion établie'
+            ]);
+        }
     }
 }
